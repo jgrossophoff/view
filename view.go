67b0c0e95fe017fd5d44f115e185434462ebce2d
Jens Groß Ophoff <jens@grossophoff.de>
2016-01-14 10:16:30 +0100

    Change template names to be relative to the template folder without extension.

--- view.go
+++ view.go
@@ -1,6 +1,12 @@
 // Package view can be used to parse all templates inside a folder with
 // a matching file extension.
 //
+// Templates are named by their path relative to the template folder without the
+// file extension. A template file inside templates/foo/bar/index.tmpl for example can be
+// executed by the name foo/bar/index.
+//
+// That way, blocks introduced in Go 1.6 can be used to achieve template extension with ease.
+//
 // It allows safe reparsing of all files on each template execution to remove
 // the need for server restarts during development.
 package view
@@ -8,9 +14,11 @@ package view
 import (
 	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"sync"
 )
 
@@ -66,14 +74,18 @@ func (v *Views) Execute(w io.Writer, data interface{}) error {
 // the folder path.
 // Save for concurrent use.
 func (v *Views) ParseTemplates() error {
-	var files []string
+	t := template.New("all")
 
 	err := filepath.Walk(v.path, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			log.Println("Err while walking template dir: ", err)
 		}
 		if !info.IsDir() && filepath.Ext(path) == v.tmplExt {
-			files = append(files, path)
+			b, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			t, err = t.New(v.TemplateName(path)).Parse(string(b))
 		}
 
 		return err
@@ -82,13 +94,20 @@ func (v *Views) ParseTemplates() error {
 		return err
 	}
 
-	t, err := template.ParseFiles(files...)
-	if err != nil {
-		return err
-	}
-
 	v.mu.Lock()
 	defer v.mu.Unlock()
 	v.tmpls = t
 	return nil
+}
+
+// TemplateName returns the file name relative to the template folder
+// without the template file extension or leading slashes.
+func (v *Views) TemplateName(s string) string {
+	// Folder name without dots or (back-)slashes
+	cf := strings.Trim(v.path, "./\\")
+	// Remove parent folder and leading (back-)slashes
+	s = strings.TrimLeft(strings.Replace(s, cf, "", 1), "/\\")
+
+	// Return without file ext
+	return s[:len(s)-len(v.tmplExt)]
 }
